28017b55d2e5772080e83e0d16bd0bae01b5cb3d
vineetn08 <vineetn23@gmail.com>
2023-05-25 21:40:35 +0530

    Plate UI Canvas

--- Assets/Scripts/PlateKitchenObject.cs
+++ Assets/Scripts/PlateKitchenObject.cs
@@ -46,4 +46,10 @@ public class PlateKitchenObject : KitchenObject
         }
 
     }
+
+    public List<KitchenObjectSO> GetKitchenObjectSOList()
+    {
+        return kitchenObjectSOList;
+    }
+
 }
